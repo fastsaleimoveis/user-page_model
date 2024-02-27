1f8891f691f5c1cd8ae77d04db8d619f41e4189a
Dauglis Moreira <dauglisc@gmail.com>
2024-02-27 08:21:45 -0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Poppins } from 'next/font/google'
 import './globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import StyledComponentsRegistry from './lib/registry';
 
-const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight:["300", "400", "500", "600", "700"]
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -17,8 +20,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html lang="pt-BR">
+      <body className={poppins.className}>
         <StyledComponentsRegistry>
           {children}
         </StyledComponentsRegistry>
