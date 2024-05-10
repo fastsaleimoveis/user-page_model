428c55d179b1d8256f9b9b337831c343efe9eb02
Dauglis Moreira <dauglisc@gmail.com>
2024-05-10 16:50:34 -0300

    fix

--- src/app/components/ScriptInjector.jsx
+++ src/app/components/ScriptInjector.jsx
@@ -2,22 +2,23 @@ import { useEffect } from 'react';
 
 const ScriptInjector = ({ scriptContent }) => {
   useEffect(() => {
-    // Cria um elemento div para parsear a string
     const container = document.createElement('div');
     container.innerHTML = scriptContent;
-
-    // Itera sobre os filhos e adiciona ao head
+  
     Array.from(container.children).forEach((child) => {
       document.head.appendChild(child.cloneNode(true));
     });
-
+  
     return () => {
-      // Remove os scripts ao desmontar o componente
       Array.from(container.children).forEach((child) => {
-        document.head.removeChild(child);
+        // Verifica se o nó pai é document.head antes de tentar removê-lo
+        if (child.parentNode === document.head) {
+          document.head.removeChild(child);
+        }
       });
     };
   }, [scriptContent]);
+  
 
   return null;
 };
